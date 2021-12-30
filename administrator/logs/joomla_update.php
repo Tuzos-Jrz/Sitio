b8486b209bc3cdfeb901c03362b928eb2ccc1758
Licgallegosmkt <96800394+Licgallegosmkt@users.noreply.github.com>
2021-12-29 17:51:52 -0700

    start
    tuzos
    
    Co-Authored-By: Jacobo Roy <38972571+jacoboroy@users.noreply.github.com>

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-12-28 21:48:03 UTC
+#Software: Joomla! 4.0.4 Stable [ Furaha ] 24-October-2021 19:25 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-12-28T21:48:03+00:00	INFO 201.146.175.144	update	Actualización inciciadoa por el usuario Club Pachuca Jrz (42). La versión antigua es la &#x200E;4.0.4.
+2021-12-28T21:48:04+00:00	INFO 201.146.175.144	update	Descargando el archivo desde https://downloads.joomla.org/cms/joomla4/4-0-5/Joomla_4.0.5-Stable-Update_Package.zip.
+2021-12-28T21:48:06+00:00	INFO 201.146.175.144	update	El archivo Joomla_4.0.5-Stable-Update_Package.zip se ha descargado.
+2021-12-28T21:48:07+00:00	INFO 201.146.175.144	update	Iniciando la instalación de la versión nueva.
+2021-12-28T21:48:12+00:00	INFO 201.146.175.144	update	Finalizando la instalación.
+2021-12-28T21:48:12+00:00	INFO 201.146.175.144	update	Borrando los archivos y carpetas que han sido eliminados.
+2021-12-28T21:48:13+00:00	INFO 201.146.175.144	update	Limpieza posterior a la instalación.
+2021-12-28T21:48:13+00:00	INFO 201.146.175.144	update	La actualización a la versión 4.0.5 se ha completado.
